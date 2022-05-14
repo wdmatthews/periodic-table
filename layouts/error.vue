befe21d064245b09def6c405313bd997548a0fde
Wesley Matthews <81110199+wdmatthews@users.noreply.github.com>
2022-05-14 13:21:19 -0500

    Project setup

---
+++ layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Error {{ error.statusCode }}
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  head: vm => ({
+    title: `Error ${vm.error.statusCode}`,
+  }),
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,45 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  ssr: false,
+  target: 'static',
+  router: {
+    base: '/periodic-table/',
+  },
+  head: {
+    title: 'Periodic Table',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+  },
+  css: [],
+  plugins: [],
+  components: true,
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify',
+  ],
+  modules: [],
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
+  },
+  build: {},
+}
